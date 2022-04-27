949281a6bbbe042d60c709bec1f895febb7b55bd
xbayitan <100149318+xbayitan@users.noreply.github.com>
2022-04-27 08:26:25 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+    basic.pause(100)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.clear_screen()
+    basic.pause(100)
+    
+
---
+++ main.ts
@@ -0,0 +1,8 @@
+while (true) {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.clearScreen()
+    basic.pause(100)
+}
